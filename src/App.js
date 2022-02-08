d38daf39382ed5bd7ec2b9daf59750391a48610b
lopera.william@gmail.com <William>
2022-02-08 10:56:51 -0500

    estilos - componentes de estilos - modulo de estilos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+import "./App.css";
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: "¡Haz todos los ejercicios!", id: "g1" },
+    { text: "¡Termina el curso!", id: "g2" },
   ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
       updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
       return updatedGoals;
     });
   };
 
   let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: "center" }}>
+      No se han encontrado objetivos. ¿Quizás agregar uno?
+    </p>
   );
 
   if (courseGoals.length > 0) {
@@ -47,7 +49,7 @@ const App = () => {
             items={courseGoals}
             onDeleteItem={deleteItemHandler}
           />
-        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        ) // <p style={{ textAlign: 'center' }}>No se han encontrado objetivos. ¿Quizás agregar uno?</p>
         } */}
       </section>
     </div>
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,27 +1,119 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+// import styled from "styled-components";
+import styles from "./CourseInput.module.css";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  const goalInputChangeHandler = event => {
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
+
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+
+//   // &.invalid input {
+//   //   border-color: red;
+//   //   background-color: #ffd7d7;
+//   // }
+
+//   // &.invalid label {
+//   //   color: red;
+//   // }
+// `;
+
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
+    setEnteredValue("");
     props.onAddGoal(enteredValue);
   };
 
   return (
+    // <form onSubmit={formSubmitHandler}>
+    //   <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+    //     <label>Meta del curso</label>
+    //     <input
+    //       type="text"
+    //       value={enteredValue}
+    //       onChange={goalInputChangeHandler}
+    //     />
+    //   </div>
+    //   <Button type="submit">Agregar</Button>
+    // </form>
+
+    // <form onSubmit={formSubmitHandler}>
+    //   <FormControl className={!isValid && "invalid"}>
+    //     <label>Meta del curso</label>
+    //     <input
+    //       type="text"
+    //       value={enteredValue}
+    //       onChange={goalInputChangeHandler}
+    //     />
+    //   </FormControl>
+    //   <Button type="submit">Agregar</Button>
+    // </form>
+
+    // <form onSubmit={formSubmitHandler}>
+    //   <FormControl invalid={!isValid}>
+    //     <label>Meta del curso</label>
+    //     <input
+    //       type="text"
+    //       value={enteredValue}
+    //       onChange={goalInputChangeHandler}
+    //     />
+    //   </FormControl>
+    //   <Button type="submit">Agregar</Button>
+    // </form>
+
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
+        <label>Meta del curso</label>
+        <input
+          type="text"
+          value={enteredValue}
+          onChange={goalInputChangeHandler}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit">Agregar</Button>
     </form>
   );
 };
